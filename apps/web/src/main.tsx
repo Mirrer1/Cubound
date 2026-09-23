667a06280425c8cbdb4f65ce6d741fda253e425f
mirrer <alsejr1004@gmail.com>
2026-09-24 02:18:44 +0900

    perf: 그리기 전에 언어 정하기

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '@/App'
 import ErrorBoundary from '@/components/ui/ErrorBoundary'
+import { useSettingsStore } from '@/store/settingsStore'
+
+document.documentElement.lang = useSettingsStore.getState().language
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
